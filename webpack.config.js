72bd262a893a09cc8bf394c9c6f7a7f4d8ff20c3
rybakN <dnb4949@gmail.com>
2022-08-12 22:43:13 +0300

    init: start task RS-Lang

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/main.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+              test: /\.tsx?$/,
+              loader: 'ts-loader',
+            },
+            {
+              test: /\.(png|jpe?g|gif)$/i,
+              loader: 'file-loader',
+              options: {
+                name: 'pictures/[name].[ext]',
+              },
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+          filename: 'style.css',
+        }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
