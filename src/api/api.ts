42ebce3c7daaa94dc08c64544ed1c4b87f18a15a
rybakN <dnb4949@gmail.com>
2022-08-18 17:07:44 +0300

    feat: add all method API request

---
+++ src/api/api.ts
@@ -0,0 +1,294 @@
+import {
+    CreateUserResponse,
+    CreateUserBody,
+    SingInResponse,
+    Word,
+    GetNewToken,
+    CreateUserWord,
+    CreateUserWordResponse,
+    GetUserAggregateWordResponse,
+    UserWord,
+    StatisticRequestBody,
+    UserStatisticResponse, UserSettingsResponse, SettingsRequestBody, Filter,
+} from './typeApi';
+
+export class Api {
+    private static baseLink = 'https://rs-lang-team-116.herokuapp.com/';
+    static async createUser(user: CreateUserBody): Promise<CreateUserResponse> {
+        const rawResponse = await fetch(this.baseLink + 'users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        const createUserResponse: CreateUserResponse = await rawResponse.json();
+        console.log(createUserResponse);
+        return createUserResponse;
+    }
+
+    static async singIn(user: CreateUserBody): Promise<SingInResponse> {
+        const rawResponse = await fetch(this.baseLink + 'signin', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user),
+        });
+        const singInResponse: SingInResponse = await rawResponse.json();
+        console.log(singInResponse);
+        return singInResponse;
+    }
+
+    static async getWords(groupNum: number, pageNum: number): Promise<Word[]> {
+        const rawResponse = await fetch(this.baseLink + `words?group=${groupNum}&page=${pageNum}`);
+        const getWordsResponse: Word[] = await rawResponse.json();
+        console.log(getWordsResponse);
+        return getWordsResponse;
+    }
+
+    static async getWord(wordId: string): Promise<Word> {
+        const rawResponse = await fetch(this.baseLink + `words/${wordId}`);
+        const getWordResponse: Word = await rawResponse.json();
+        console.log(getWordResponse);
+        return getWordResponse;
+    }
+
+    static async getUser(userId: string, token: string): Promise<CreateUserResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getUserResponse: CreateUserResponse = await rawResponse.json();
+        console.log(getUserResponse);
+        return getUserResponse;
+    }
+
+    static async updateUser(userId: string, token: string, requestBody: CreateUserBody): Promise<CreateUserResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody),
+        });
+        const updateUserResponse: CreateUserResponse = await rawResponse.json();
+        console.log(updateUserResponse);
+        return updateUserResponse;
+    }
+
+    static async deleteUser(userId: string, token: string): Promise<void> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+    }
+
+    static async getNewUserToken(userId: string, refreshToken: string): Promise<GetNewToken> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/tokens`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${refreshToken}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getNewUserTokenResponse: GetNewToken = await rawResponse.json();
+        console.log(getNewUserTokenResponse);
+        return getNewUserTokenResponse;
+    }
+
+    static async getUserWords(userId: string, token: string): Promise<CreateUserWordResponse[]> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/words`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getUserWordsResponse: CreateUserWordResponse[] = await rawResponse.json();
+        console.log(getUserWordsResponse);
+        return getUserWordsResponse;
+    }
+
+    static async createUserWord(userId: string, token: string, wordId: string, requestBody: CreateUserWord): Promise<CreateUserWordResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/words/${wordId}`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody),
+        });
+        const createUserWordResponse: CreateUserWordResponse = await rawResponse.json();
+        console.log(createUserWordResponse);
+        return createUserWordResponse;
+    }
+
+    static async getUserWordById(userId: string, token: string, wordId: string): Promise<CreateUserWordResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/words/${wordId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getUserWordByIdResponse: CreateUserWordResponse = await rawResponse.json();
+        console.log(getUserWordByIdResponse);
+        return getUserWordByIdResponse;
+    }
+
+    static async updateUserWord(userId: string, token: string, wordId: string, requestBody: CreateUserWord): Promise<CreateUserWordResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/words/${wordId}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody),
+        });
+        const updateUserWordResponse: CreateUserWordResponse = await rawResponse.json();
+        console.log(updateUserWordResponse);
+        return updateUserWordResponse;
+    }
+
+    static async deleteUserWordById(userId: string, token: string, wordId: string): Promise<void> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/words/${wordId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+    }
+
+    static async getUserAggregateWord(
+        userId: string,
+        token: string,
+        pageNum: number,
+        wordsPerPage: number,
+        filter?: Filter,
+        groupNum?: number
+    ): Promise<GetUserAggregateWordResponse> {
+        const link: string = this.createLink(pageNum, wordsPerPage, filter, groupNum);
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/aggregatedWords?` + link, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getUserAggregateWordResponse: GetUserAggregateWordResponse = await rawResponse.json();
+        console.log(getUserAggregateWordResponse);
+        return getUserAggregateWordResponse;
+    }
+
+    static async getUserAggregateWordById(userId: string, token: string, wordId: string): Promise<Array<UserWord>> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/aggregatedWords/${wordId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getUserAggregateWordByIdResponse: Array<UserWord> = await rawResponse.json();
+        console.log(getUserAggregateWordByIdResponse);
+        return getUserAggregateWordByIdResponse;
+    }
+
+    static async getUserStatistic(userId: string, token: string): Promise<UserStatisticResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/statistics`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getUserStatisticResponse: UserStatisticResponse = await rawResponse.json();
+        console.log(getUserStatisticResponse);
+        return getUserStatisticResponse;
+    }
+
+    static async upsertUserStatistic(userId: string, token: string, requestBody: StatisticRequestBody): Promise<UserStatisticResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/statistics`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody),
+        });
+        const upsetUserStatisticResponse: UserStatisticResponse = await rawResponse.json();
+        console.log(upsetUserStatisticResponse);
+        return upsetUserStatisticResponse;
+    }
+
+    static async getUserSettings(userId: string, token: string): Promise<UserSettingsResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/settings`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        const getUserSettingsResponse: UserSettingsResponse = await rawResponse.json();
+        console.log(getUserSettingsResponse);
+        return getUserSettingsResponse;
+    }
+
+    static async upsertUserSettings(userId: string, token: string, requestBody: SettingsRequestBody): Promise<UserSettingsResponse> {
+        const rawResponse = await fetch(this.baseLink + `users/${userId}/settings`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody),
+        });
+        const upsertUserSettingsResponse: UserSettingsResponse = await rawResponse.json();
+        console.log(upsertUserSettingsResponse);
+        return upsertUserSettingsResponse;
+    }
+
+    private static createLink(
+        pageNum: number,
+        wordsPerPage: number,
+        filter?: Filter,
+        groupNum?: number
+    ): string {
+        let link: string;
+        if (groupNum == undefined) {
+            link = `page=${pageNum}&wordsPerPage=${wordsPerPage}&filter=${JSON.stringify(filter.condition)}`;
+        } else {
+            link = `group=${groupNum}&page=${pageNum}&wordsPerPage=${wordsPerPage}&filter=${JSON.stringify(filter.condition)}`;
+        }
+        return link;
+    }
+}
+
+
+
+
---
+++ src/api/typeApi.ts
@@ -0,0 +1,132 @@
+export interface CreateUserBody {
+    email: string
+    password: string
+}
+
+export interface CreateUserResponse {
+    id: string
+    email: string
+}
+
+export interface SingInResponse {
+    message: string
+    token: string
+    refreshToken: string
+    userId: string
+}
+
+export interface Word {
+    id: string
+    group: number
+    page: number
+    word: string
+    image: string
+    audio: string
+    audioMeaning: string
+    audioExample: string
+    textMeaning: string
+    textExample: string
+    transcription: string
+    textExampleTranslate: string
+    textMeaningTranslate: string
+    wordTranslate: string
+}
+
+export interface GetNewToken {
+    token: string
+    refreshToken: string
+}
+
+export class CreateUserWord {
+    difficulty: string
+    optional: CreateUserWordOption
+}
+
+export interface CreateUserWordResponse {
+    id: string
+    difficulty: string
+    optional: CreateUserWordOption
+    wordId: string
+}
+
+export interface CreateUserWordOption {
+    testFieldString: string
+    testFieldBoolean: boolean
+}
+
+export type GetUserAggregateWordResponse = Root2[]
+
+export interface Root2 {
+    paginatedResults: UserWord[]
+    totalCount: TotalCount[]
+}
+
+export interface UserWord {
+    _id: string
+    group: number
+    page: number
+    word: string
+    image: string
+    audio: string
+    audioMeaning: string
+    audioExample: string
+    textMeaning: string
+    textExample: string
+    transcription: string
+    textExampleTranslate: string
+    textMeaningTranslate: string
+    wordTranslate: string
+    userWord?: CreateUserWord
+}
+
+export interface TotalCount {
+    count: number
+}
+
+export interface UserStatisticResponse {
+    id: string
+    learnedWords: number
+    optional?: any
+}
+
+export interface StatisticRequestBody {
+    learnedWords: number
+    optional?: any
+}
+
+export interface UserSettingsResponse {
+    id: string
+    wordsPerDay: number
+    optional?: any
+}
+
+export interface SettingsRequestBody {
+    wordsPerDay: number
+    optional?: any
+}
+
+export class UserWordFilter {
+    "userWord.difficulty": string;
+    "userWord.optional.repeat": boolean;
+}
+
+export class OrCondition {
+    $or: Array<UserWordFilter | OrCondition | AndCondition>;
+    constructor(or: Array<UserWordFilter | OrCondition | AndCondition>) {
+        this.$or = or;
+    }
+}
+
+export class AndCondition {
+    $and: Array<UserWordFilter | OrCondition | AndCondition>;
+    constructor(and: Array<UserWordFilter | OrCondition | AndCondition>) {
+        this.$and = and;
+    }
+}
+
+export class Filter {
+    condition: OrCondition | AndCondition;
+    constructor(condition: OrCondition | AndCondition) {
+        this.condition = condition;
+    }
+}
