2915db4a8a0849dcc9656ac118fbf61bca877112
Aleksandr Lobkov <das149848@gmail.com>
2022-08-19 20:50:43 +0400

    feat: add authorization section

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,6 @@
-import './pages/index.css';
+import './index.css';
 import './pictures/header-background1.png';
+import "./pictures/textbook.png";
+import "./pictures/statistics.png";
+import "./pictures/game.png";
+import "./pictures/dictionary.png";
