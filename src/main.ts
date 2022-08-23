cfc65179c52ccfdb53561df17fa1971024fc59d3
Aleksandr Lobkov <das149848@gmail.com>
2022-08-23 22:42:13 +0400

    feat: add styles for active and noactive views of sidebar

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,5 @@ import './pictures/profile-picture.jpg';
 import './pictures/git-logo.png';
 import './components/create-sidebar/create-sidebar.css';
 import createSideBar from './components/create-sidebar/create-sidebar';
-import { registration } from './components/auth';
-
 
 createSideBar();
-const SINGBUTTON = document.querySelector('.sign-in-or-log-in-button');
-
-SINGBUTTON.addEventListener('click', registration);
