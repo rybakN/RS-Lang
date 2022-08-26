42904646c6dbd8e7cf9a4f28bc214bf37599ed39
rybakN <dnb4949@gmail.com>
2022-08-26 14:42:13 +0300

    feat: add btn StopListen

--- src/components/viewWordCards/wordCardBtnHandler/handlerCombiner.ts
+++ src/components/viewWordCards/wordCardBtnHandler/handlerCombiner.ts
@@ -7,6 +7,7 @@ import { RestoreBtn } from './restoreBtn';
 import { ToggleBtnName } from './utilsWordCard/toggleBtnName';
 import { UpdateUserStatistic } from './utilsWordCard/updateUserStatistic';
 import { CreateUserWordBody } from './utilsWordCard/createUserWordBody';
+import { StopListenBtn } from './stopListenBtn';
 
 export class HandlerCombiner {
     btn: Map<string, BtnHandler> = new Map();
@@ -17,11 +18,12 @@ export class HandlerCombiner {
         this.toggleBtnName = new ToggleBtnName();
         this.updateUserStat = new UpdateUserStatistic();
         this.userWordBody = new CreateUserWordBody();
-        this.btn.set(ListenBtn.btnName, new ListenBtn());
+        this.btn.set(ListenBtn.btnName, new ListenBtn(this.toggleBtnName));
         this.btn.set(DifficultBtn.btnName, new DifficultBtn(this.toggleBtnName, this.updateUserStat, this.userWordBody));
         this.btn.set(EasyBtn.btnName, new EasyBtn(this.toggleBtnName, this.userWordBody));
         this.btn.set(LearningBtn.btnName, new LearningBtn(this.toggleBtnName, this.userWordBody));
         this.btn.set(RestoreBtn.btnName, new RestoreBtn(this.toggleBtnName, this.updateUserStat, this.userWordBody));
+        this.btn.set(StopListenBtn.btnName, new StopListenBtn(this.toggleBtnName));
     }
     handler(btnName: string, wordId: string): void {
         const handler = this.btn.get(btnName);
