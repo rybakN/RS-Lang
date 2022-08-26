5e080d086305df10cf3b147b60a7ef234eeaf1d9
rybakN <dnb4949@gmail.com>
2022-08-26 10:19:53 +0300

    feat: refactoring wordCard

--- src/components/viewWordCards/wordCardBtnHandler/difficultBtn.ts
+++ src/components/viewWordCards/wordCardBtnHandler/difficultBtn.ts
@@ -1,33 +1,48 @@
 import { BtnHandler } from './typeBtnHandler';
 import { Api } from '../../../api/api';
-import { CreateUserWord } from '../../../api/typeApi';
-import { ToggleBtnName } from './toggleBtnName';
+import { CreateUserWord, StatisticRequestBody, UserStatisticResponse, UserWord } from '../../../api/typeApi';
+import { ToggleBtnName } from './utilsWordCard/toggleBtnName';
+import { UpdateUserStatistic } from './utilsWordCard/updateUserStatistic';
 
 export  class DifficultBtn implements BtnHandler {
     static btnName = 'difficult';
     toggleBtnName: ToggleBtnName;
-    constructor(toggleBtnName: ToggleBtnName) {
+    userStat: UpdateUserStatistic;
+    constructor(toggleBtnName: ToggleBtnName, updateUserStat: UpdateUserStatistic) {
         this.toggleBtnName = toggleBtnName;
+        this.userStat = updateUserStat;
     }
     async handle(wordId: string): Promise<void> {
         const userId: string = localStorage.getItem('userId');
         const token: string = localStorage.getItem('userToken');
+        const wordCard: HTMLElement = document.getElementById(`${wordId}`);
+        const response = await Api.getUserAggregateWordById(userId, token, wordId).then();
         const requestBody: CreateUserWord = {
             difficulty: 'hard',
             optional: {
                 learning: false,
+                statistic: {
+                    row: 1,
+                    correct: 1,
+                    incorrect: response[0].userWord.optional.statistic.incorrect
+                }
             }
         }
 
-        const wordCard: HTMLElement = document.getElementById(`${wordId}`);
         if (wordCard.classList.contains('bg-success')) {
             await Api.updateUserWord(userId, token, wordId, requestBody);
             wordCard.classList.remove('bg-success');
             wordCard.classList.add('bg-danger');
             this.toggleBtnName.toggleBtnName(wordCard, 'restore', 'learned');
         } else {
-            await Api.createUserWord(userId, token, wordId, requestBody);
-            wordCard.classList.add('bg-danger');
+            const response = await Api.getUserAggregateWordById(userId, token, wordId);
+            if (response[0]['userWord'] != undefined) {
+                await Api.updateUserWord(userId, token, wordId, requestBody);
+                wordCard.classList.add('bg-danger');
+            } else {
+                await Api.createUserWord(userId, token, wordId, requestBody);
+                wordCard.classList.add('bg-danger');
+            }
         }
 
         wordCard.querySelectorAll('button').forEach((item: HTMLButtonElement) => {
@@ -35,5 +50,6 @@ export  class DifficultBtn implements BtnHandler {
         })
 
         wordCard.parentElement.classList.remove('bg-success');
+        this.userStat.updateUserStatistic(userId, token).then();
     }
 }
---
+++ src/components/viewWordCards/wordCardBtnHandler/utilsWordCard/updateUserStatistic.ts
@@ -0,0 +1,18 @@
+import { StatisticRequestBody, UserStatisticResponse } from '../../../../api/typeApi';
+import { Api } from '../../../../api/api';
+
+export class UpdateUserStatistic {
+    async updateUserStatistic(userId: string, token: string): Promise<void> {
+        const response: UserStatisticResponse = await Api.getUserStatistic(userId, token);
+        let quantityLearnedWords: number;
+        response.learnedWords > 0 ? quantityLearnedWords = 1 : quantityLearnedWords = 0;
+        const requestBodyStatistic: StatisticRequestBody = {
+            learnedWords: response.learnedWords - quantityLearnedWords,
+            optional: {
+                learnedWordsByDays: response.optional.learnedWordsByDays,
+                games: response.optional.games,
+            }
+        }
+        Api.upsertUserStatistic(userId, token, requestBodyStatistic).then();
+    }
+}
