f986a4f207455cb95eaafc26fa8fb93f6b671bb6
rybakN <dnb4949@gmail.com>
2022-08-26 11:42:03 +0300

    refactor: refactoring btnHandler

--- src/components/viewWordCards/wordCardBtnHandler/handlerCombiner.ts
+++ src/components/viewWordCards/wordCardBtnHandler/handlerCombiner.ts
@@ -6,19 +6,22 @@ import { LearningBtn } from './learningBtn';
 import { RestoreBtn } from './restoreBtn';
 import { ToggleBtnName } from './utilsWordCard/toggleBtnName';
 import { UpdateUserStatistic } from './utilsWordCard/updateUserStatistic';
+import { CreateUserWordBody } from './utilsWordCard/createUserWordBody';
 
 export class HandlerCombiner {
     btn: Map<string, BtnHandler> = new Map();
     toggleBtnName: ToggleBtnName;
     updateUserStat: UpdateUserStatistic;
+    userWordBody: CreateUserWordBody;
     constructor() {
         this.toggleBtnName = new ToggleBtnName();
         this.updateUserStat = new UpdateUserStatistic();
+        this.userWordBody = new CreateUserWordBody();
         this.btn.set(ListenBtn.btnName, new ListenBtn());
-        this.btn.set(DifficultBtn.btnName, new DifficultBtn(this.toggleBtnName, this.updateUserStat));
-        this.btn.set(EasyBtn.btnName, new EasyBtn(this.toggleBtnName));
-        this.btn.set(LearningBtn.btnName, new LearningBtn(this.toggleBtnName));
-        this.btn.set(RestoreBtn.btnName, new RestoreBtn(this.toggleBtnName, this.updateUserStat));
+        this.btn.set(DifficultBtn.btnName, new DifficultBtn(this.toggleBtnName, this.updateUserStat, this.userWordBody));
+        this.btn.set(EasyBtn.btnName, new EasyBtn(this.toggleBtnName, this.userWordBody));
+        this.btn.set(LearningBtn.btnName, new LearningBtn(this.toggleBtnName, this.userWordBody));
+        this.btn.set(RestoreBtn.btnName, new RestoreBtn(this.toggleBtnName, this.updateUserStat, this.userWordBody));
     }
     handler(btnName: string, wordId: string): void {
         const handler = this.btn.get(btnName);
