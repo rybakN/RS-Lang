d3f61bdbb9b7aa09f2630ee5736375e8484138f8
Aleksandr Lobkov <das149848@gmail.com>
2022-08-28 20:44:09 +0400

    feat: add favicon and function which create header on textbook page

--- src/pages/textbook/textbook.ts
+++ src/pages/textbook/textbook.ts
@@ -1,1 +1,6 @@
 import './textbook.css';
+import '../../pictures/header-background.png';
+import '../../pictures/favicon.png';
+import createHeader from '../../components/create-header/create-header';
+
+createHeader('Textbook', false)
