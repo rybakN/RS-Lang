430d519157c6840420338e52cf06c9fa10df645c
Aleksandr Lobkov <das149848@gmail.com>
2022-08-28 21:02:04 +0400

    feat: link function auth to button sign in / log in on textbook page

--- src/pages/textbook/textbook.ts
+++ src/pages/textbook/textbook.ts
@@ -1,6 +1,17 @@
 import './textbook.css';
 import '../../pictures/header-background.png';
 import '../../pictures/favicon.png';
+import { registration, logOut } from '../../components/auth';
 import createHeader from '../../components/create-header/create-header';
 
-createHeader('Textbook', false)
+createHeader('Textbook', false);
+
+let  ENTEREXITBUTTON: Element;
+if(document.querySelector('.sign-in-or-log-in-button')){
+  ENTEREXITBUTTON = document.querySelector('.sign-in-or-log-in-button')
+  ENTEREXITBUTTON.addEventListener('click', registration);
+}
+else{
+  ENTEREXITBUTTON = document.querySelector('.log-out');
+  ENTEREXITBUTTON.addEventListener('click', logOut);
+}
